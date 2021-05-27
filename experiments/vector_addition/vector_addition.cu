cf960ac84abcaed6877012c01613cc7fbe679fb7
jarusified <jarus3001@gmail.com>
2021-05-27 12:14:16 -0700

    Make vector_addtion work with activity trace async

--- experiments/vector_addition/vector_addition.cu
+++ experiments/vector_addition/vector_addition.cu
@@ -1,8 +1,36 @@
 #include <stdio.h>
 #include <iostream>
+#include <cuda.h>
+#include <cupti.h>
+#include <cuda_runtime.h>
+
+#define DRIVER_API_CALL(apiFuncCall)                                           \
+do {                                                                           \
+    CUresult _status = apiFuncCall;                                            \
+    if (_status != CUDA_SUCCESS) {                                             \
+        fprintf(stderr, "%s:%d: error: function %s failed with error %d.\n",   \
+                __FILE__, __LINE__, #apiFuncCall, _status);                    \
+        exit(-1);                                                              \
+    }                                                                          \
+} while (0)
+
+#define RUNTIME_API_CALL(apiFuncCall)                                          \
+do {                                                                           \
+    cudaError_t _status = apiFuncCall;                                         \
+    if (_status != cudaSuccess) {                                              \
+        fprintf(stderr, "%s:%d: error: function %s failed with error %s.\n",   \
+                __FILE__, __LINE__, #apiFuncCall, cudaGetErrorString(_status));\
+        exit(-1);                                                              \
+    }                                                                          \
+} while (0)
+
+#define USE_CUPTI 1
+
+extern void initTrace(void);
+extern void finiTrace(void);
 
 // Size of array = 1M or 2**20
-#define N 1048576
+#define N 1048576*256
 
 // Kernel function to add the elements of two arrays
 __global__ void add_vectors(float *a, float *b, float *c)
@@ -13,6 +41,13 @@ __global__ void add_vectors(float *a, float *b, float *c)
 
 // Main program
 int main(void) {
+	// initialize the activity trace
+	// make sure activity is enabled before any CUDA API
+	#if USE_CUPTI
+		initTrace();
+		DRIVER_API_CALL(cuInit(0));
+	#endif
+
 	// Number of bytes to allocate for N integers
 	size_t bytes = N*sizeof(float);
 
@@ -23,9 +58,9 @@ int main(void) {
 
 	// Allocate memory for arrays d_A, d_B, and d_C on device
 	float *d_A, *d_B, *d_C;
-	cudaMalloc(&d_A, bytes);
-	cudaMalloc(&d_B, bytes);
-	cudaMalloc(&d_C, bytes);
+	RUNTIME_API_CALL(cudaMalloc(&d_A, bytes));
+	RUNTIME_API_CALL(cudaMalloc(&d_B, bytes));
+	RUNTIME_API_CALL(cudaMalloc(&d_C, bytes));
 
 	// Fill host arrays A and B
 	for(int i = 0; i < N; i++){
@@ -34,8 +69,8 @@ int main(void) {
 	}
 
 	// Copy data from host arrays A and B to device arrays d_A and d_B
-	cudaMemcpy(d_A, A, bytes, cudaMemcpyHostToDevice);
-	cudaMemcpy(d_B, B, bytes, cudaMemcpyHostToDevice);
+	RUNTIME_API_CALL(cudaMemcpy(d_A, A, bytes, cudaMemcpyHostToDevice));
+	RUNTIME_API_CALL(cudaMemcpy(d_B, B, bytes, cudaMemcpyHostToDevice));
 
 	// Set execution configuration parameters
 	//		blockSize: number of CUDA threads per grid block
@@ -47,7 +82,7 @@ int main(void) {
 	add_vectors<<< numBlocks, blockSize >>>(d_A, d_B, d_C);
 
 	// Copy data from device array d_C to host array C
-	cudaMemcpy(C, d_C, bytes, cudaMemcpyDeviceToHost);
+	RUNTIME_API_CALL(cudaMemcpy(C, d_C, bytes, cudaMemcpyDeviceToHost));
 
 	// Check for errors (all values should be 3.0f)
 	float maxError = 0.0f;
@@ -67,9 +102,9 @@ int main(void) {
 	free(C);
 
 	// Free GPU memory
-	cudaFree(d_A);
-	cudaFree(d_B);
-	cudaFree(d_C);
+	RUNTIME_API_CALL(cudaFree(d_A));
+	RUNTIME_API_CALL(cudaFree(d_B));
+	RUNTIME_API_CALL(cudaFree(d_C));
 
   	printf("\n---------------------------\n");
 	printf("__RESULTS__\n");
@@ -79,6 +114,12 @@ int main(void) {
 	printf("Blocks In Grid    = %d\n", numBlocks);
 	printf("Error             = %f\n", maxError);
   	printf("---------------------------\n\n");
+
+	#if USE_CUPTI
+	  	// Flush all remaining CUPTI buffers before resetting the device.
+	  	// This can also be called in the cudaDeviceReset callback.
+	  	cuptiActivityFlushAll(0);
+  	#endif
 
 	return 0;
 }
