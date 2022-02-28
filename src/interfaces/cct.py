c888a141f72d5caa7c7e885a6347d704af93efa3
jarusified <jarus3001@gmail.com>
2022-02-28 06:39:37 +0530

    Add labels to the cct nodes

--- src/interfaces/cct.py
+++ src/interfaces/cct.py
@@ -4,14 +4,14 @@ import pandas as pd
 import networkx as nx
 from logger import get_logger
 from utils.sanitizer import Sanitizer
-from utils.df import df_factorize_column, df_add_column
+from utils.df import df_factorize_column, df_add_column, df_lookup_by_column
 from networkx.readwrite import json_graph
 
 LOGGER = get_logger(__name__)
 
 class CCT():
     FILENAME = 'region_profile.json'
-    COLUMNS = ["time (inc)", "time", "name", "module"]
+    COLUMNS = ["time (inc)", "time", "label", "class"]
 
     def __init__(self, data_dir):
         LOGGER.info(f"{type(self).__name__} interface triggered.")
@@ -20,7 +20,7 @@ class CCT():
         self.graph = nx.DiGraph()
         self.hts = { exp: ht.GraphFrame.from_caliper_json(self.file_paths[exp]) for exp in self.experiments }
         self.dfs = { exp: self.add_path_columns(self.hts[exp]) for exp in self.experiments }
-        self.nxgs = { exp: self.ht_graph_to_nxg(self.hts[exp].graph) for exp in self.experiments }
+        self.nxgs = { exp: self.ht_graph_to_nxg(self.hts[exp]) for exp in self.experiments }
 
     def get_idx(self, callsite):
         return self.callsite2idx[callsite]
@@ -44,7 +44,7 @@ class CCT():
 
         for node in ht.graph.traverse():
             node_name = Sanitizer.from_htframe(node.frame)
-            cs_idx = self.get_idx[node_name]
+            cs_idx = self.get_idx(node_name)
             paths[cs_idx] = [self.get_idx(Sanitizer.from_htframe(_)) for _ in node.paths()[0]]
             callers[cs_idx] = [self.get_idx(Sanitizer.from_htframe(_.frame)) for _ in node.parents]
             callees[cs_idx] = [self.get_idx(Sanitizer.from_htframe(_.frame)) for _ in node.children]
@@ -55,14 +55,16 @@ class CCT():
 
         return df
 
-    @staticmethod
-    def ht_graph_to_nxg(ht_graph) -> nx.DiGraph:
+    def ht_graph_to_nxg(self, ht) -> nx.DiGraph:
         """
         Constructs a networkX graph from hatchet graph.
         :param ht_graph: (hatchet.Graph) Hatchet Graph
         :return: (NetworkX.nxg) NetworkX graph
         """
-        assert isinstance(ht_graph, ht.graph.Graph)
+        ht_graph = ht.graph
+        ht_df = ht.dataframe
+        assert isinstance(ht_df, pd.DataFrame)
+        # assert isinstance(ht_graph, ht.graph.Graph)
 
         nxg = nx.DiGraph()
         for root in ht_graph.roots:
@@ -88,56 +90,36 @@ class CCT():
             finally:
                 del root
 
-        return nxg
-
-
-    @staticmethod
-    def _create_nxg_from_paths(paths) -> nx.DiGraph:
-        """
-        :param paths:
-        :return:
-        """
-        assert isinstance(paths, list)
-
-        nxg = nx.DiGraph()
-
-        # go over all path
-        for i, path in enumerate(paths):
-
-            # go over the callsites in this path
-            plen = len(path)
-
-            for j in range(plen - 1):
-                source = path[j]
-                target = path[j + 1]
-
-                if not nxg.has_edge(source, target):
-                    nxg.add_edge(source, target)
+        self._node_attributes(nxg, ht_df)
+        self._edge_attributes(nxg)
 
         return nxg
 
-    def _edge_attributes(self):
-        pass
-
-    def _node_attributes(self):
+    def _node_attributes(self, nxg, df):
         datamap = {}
-        for callsite in self.nxg.nodes():
+        for callsite in nxg.nodes():
             for column in CCT.COLUMNS:
                 if column not in datamap:
                     datamap[column] = {}
 
-                callsite_idx = self.sg.get_idx(callsite, "callsite")
-                _df = self.sg.df_lookup_with_column("name", callsite_idx)
+                callsite_idx = self.get_idx(callsite)
+                _df = df_lookup_by_column(df, "name", callsite_idx)
 
                 if column == "time (inc)":
-                    datamap[column][callsite] = self.get_mean_runtime(_df, self.time_inc)
+                    datamap[column][callsite] = self.get_mean_runtime(_df, column)
                 elif column == "time":
-                    datamap[column][callsite] = self.get_mean_runtime(_df, self.time_exc)
-                elif column == "name":
-                    datamap['label'][callsite] = callsite
+                    datamap[column][callsite] = self.get_mean_runtime(_df, column)
+                elif column == "label":
+                    datamap[column][callsite] = callsite
                 elif column == "module":
-                    datamap['class'][callsite] = self.sg.get_module(callsite_idx)
+                    datamap[column][callsite] = 'CPU'
 
+        for idx, key in enumerate(datamap):
+            nx.set_node_attributes(nxg, name=key, values=datamap[key])
+
+
+    def _edge_attributes(self, nxg):
+        pass
 
     def get_nxg(self, exp):
         return json_graph.node_link_data(self.nxgs[exp])
