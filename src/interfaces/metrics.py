56453f4c2885288b840ab16ba01305974ecc2896
jarusified <jarus3001@gmail.com>
2022-03-16 23:30:57 -0700

    Functions for getting the kernels from a run

--- src/interfaces/metrics.py
+++ src/interfaces/metrics.py
@@ -2,37 +2,53 @@ import os
 import pandas as pd
 
 class Metrics():
-    FILENAME = 'runtime_metrics.csv'
+    # TODO: Modify the filenames.
+    RUNTIME_SUMMARY_FILE_NAME = 'runtime_metrics.csv'
+    KERNEL_SUMMARY_FILE_NAME = 'metric_summary.csv'
 
     def __init__(self, data_dir):
         """
-        Initializes a Timeline object.
+        Initializes a Metrics object.
         """
         self.experiments = os.listdir(data_dir)
-        self.file_paths = {exp: os.path.join(os.path.abspath(data_dir), f'{exp}/{self.FILENAME}') for exp in self.experiments}
-        
+        self.runtime_summary_file_paths = {exp: os.path.join(os.path.abspath(data_dir), f'{exp}/{self.RUNTIME_SUMMARY_FILE_NAME}') for exp in self.experiments}
+        self.kernel_summary_file_paths = {exp: os.path.join(os.path.abspath(data_dir), f'{exp}/{self.KERNEL_SUMMARY_FILE_NAME}') for exp in self.experiments}
+
+        # Find the kernels from the first experiment.
+        self.kernels = self.get_kernels(self.experiments[0]);
+
         self.dfs = {}
         self.runtime_metrics = {}
         self.transfer_metrics = {}
         self.atts = {}
         self.total_runtime = {}
-        for exp in self.experiments:
-            # Check if file exists.
-            if os.path.exists(self.file_paths[exp]):
-                self.dfs[exp] = pd.read_csv(self.file_paths[exp], sep=", ", engine='python')
-                self.runtime_metrics[exp] = self.load_runtime_metrics(self.dfs[exp])
-                self.transfer_metrics[exp] = self.load_transfer_metrics(self.dfs[exp])
-                self.atts[exp] =  self.load_atts(self.dfs[exp])
-                self.total_runtime[exp] = self.load_total_time(self.dfs[exp])
-                print(self.total_runtime[exp])
 
+        # for exp in self.experiments:
+        #     # Check if the runtime metric file exists.
+        #     if os.path.exists(self.runtime_summary_file_paths[exp]):
+        #         self.dfs[exp] = pd.read_csv(self.runtime_file_paths[exp], sep=", ", engine='python')
+        #         self.runtime_metrics[exp] = self.load_runtime_metrics(self.dfs[exp])
+        #         self.transfer_metrics[exp] = self.load_transfer_metrics(self.dfs[exp])
+        #         self.atts[exp] =  self.load_atts(self.dfs[exp])
+        #         self.total_runtime[exp] = self.load_total_time(self.dfs[exp])
+    
+        #     # Check if the kernel summary file exists.
+        #     elif os.path.exists(self.kernel_summary_file_paths[exp]):
+        #         self.dfs[exp] = pd.read_csv(self.kernel_summary_file_paths[exp], sep=", ", engine='python')
+                
+    # Write a function that prints 10 most expensive kernels.
+    def print_expensive_kernels(self, exps):
+        expensive_kernels = {}
+        for exp in exps:
+            expensive_kernels[exp] = self.get_expensive_kernels(exp)
+        return expensive_kernels        
+ 
     def load_runtime_metrics(self, df):
         """
         Populates the runtime (e.g., execution time) from the csv file.
         """
         metrics = []
         for jdict in df.to_dict(orient='records'):
-            # print(jdict)
             if jdict['units'] == 'sec':
                 metrics.append(jdict)
         return metrics
@@ -71,8 +87,16 @@ class Metrics():
         return {
             'runtime_metrics': self.runtime_metrics[exp],
             'transfer_metrics': self.transfer_metrics[exp],
-            'problem_size': self.problem_size[exp]
+            'atts': self.atts[exp]
         }
+
+    def get_kernels(self, exp):
+        """
+        Returns a list of kernels for a given ensemble.
+        Note: It is assumed that the kernels are the same for all experiments.
+        """
+        df = pd.read_csv(self.kernel_summary_file_paths[exp], sep=",", engine='python', skiprows=6)
+        return df['Kernel'].unique()
 
     def sort_by_runtime(self, exps):
         return dict(sorted(self.total_runtime.items(), key=lambda item: item[1], reverse=True))
