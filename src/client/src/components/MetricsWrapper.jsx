72bc763dc5116cad05ad38f74b3208bfbdcfd743
jarusified <jarus3001@gmail.com>
2022-03-17 20:19:41 -0700

    Add d3 labels

--- src/client/src/components/MetricsWrapper.jsx
+++ src/client/src/components/MetricsWrapper.jsx
@@ -150,6 +150,48 @@ function RuntimeMetrics() {
                 .append("g")
                 .attr("class", "brush")
                 .call(brush);
+
+            // What to do when one group is hovered
+            var highlight = function(d){
+                // reduce opacity of all groups
+                d3.selectAll(".myArea").style("opacity", .1)
+                // expect the one that is hovered
+                d3.select("."+d).style("opacity", 1)
+            }
+
+            // And when it is not hovered anymore
+            var noHighlight = function(d){
+                d3.selectAll(".myArea").style("opacity", 1)
+            }
+
+            // Add one dot in the legend for each name.
+            var size = 10
+            svg.selectAll("myrect")
+                .data(kernels)
+                .enter()
+                .append("rect")
+                .attr("x", 400)
+                .attr("y", function(d,i){ return 10 + i*(size+5)}) // 100 is where the first dot appears. 25 is the distance between dots
+                .attr("width", size)
+                .attr("height", size)
+                .style("fill", function(d){ return color(d)})
+                .on("mouseover", highlight)
+                .on("mouseleave", noHighlight)
+
+            // Add one dot in the legend for each name.
+            svg.selectAll("mylabels")
+                .data(kernels)
+                .enter()
+                .append("text")
+                .attr("x", 400 + size*1.2)
+                .attr("y", function(d,i){ return 10 + i*(size+5) + (size/2)}) // 100 is where the first dot appears. 25 is the distance between dots
+                .style("fill", function(d){ return color(d)})
+                .text(function(d){ return d})
+                .attr("text-anchor", "left")
+                .style("font-size", "10px")
+                .style("alignment-baseline", "middle")
+                .on("mouseover", highlight)
+                .on("mouseleave", noHighlight)
         }
 	}, [kernelMetrics]);
 
