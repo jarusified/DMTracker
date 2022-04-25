0185af5464fc79eb57f3756f1fb33d682e9c4813
jarusified <jarus3001@gmail.com>
2022-04-25 07:53:19 -0700

    Add zero copy matrix information

--- src/interfaces/matrix.py
+++ src/interfaces/matrix.py
@@ -1,14 +1,17 @@
 import os
-from generators import H2DUnifiedMemoryCommMatrixGenerator, P2PUnifiedMemoryCommMatrixGenerator, H2DCudaMemcpyCommMatrixGenerator, P2PCudaMemcpyCommMatrixGenerator
+from generators import H2DUnifiedMemoryCommMatrixGenerator, P2PUnifiedMemoryCommMatrixGenerator, H2DCudaMemcpyCommMatrixGenerator, P2PCudaMemcpyCommMatrixGenerator, ZeroCopyInfoGenerator
 
 class Matrix():
-    FILENAME = 'gpu_trace.csv'
+    GPU_TRACE_FILENAME = 'nvprof_gpu_trace.csv'
+    METRIC_TRACE_FILENAME = 'metric_summary.csv'
 
     def __init__(self, data_dir):
         self.experiments = os.listdir(data_dir)
-        self.file_paths = [os.path.join(os.path.abspath(data_dir), f'{exp}/{self.FILENAME}') for exp in self.experiments]
+        self.gpu_trace_paths = [os.path.join(os.path.abspath(data_dir), f'{exp}/{self.GPU_TRACE_FILENAME}') for exp in self.experiments]
+        self.metric_trace_paths = [os.path.join(os.path.abspath(data_dir), f'{exp}/{self.METRIC_TRACE_FILENAME}') for exp in self.experiments]
+        
         num_devices = 4
-        for file_path in self.file_paths:
+        for file_path in self.gpu_trace_paths:
             print(file_path, os.path.exists(file_path))
             if (os.path.exists(file_path)):
                 h2d_um_memcpy_comm = H2DUnifiedMemoryCommMatrixGenerator(num_devices)
@@ -16,9 +19,9 @@ class Matrix():
                 p2p_um_memcpy_comm = P2PUnifiedMemoryCommMatrixGenerator(num_devices)
                 p2p_um_num_bytes_comm_matrix, p2p_um_num_times_comm_matrix = p2p_um_memcpy_comm.generate_comm_matrix(file_path)
             
-                print("Host-device communication matrix:")
+                print("Host-device communication matrix (unified):")
                 print(h2d_um_num_bytes_comm_matrix, h2d_um_num_times_comm_matrix)
-                print("Peer-Peer communication matrix:")
+                print("Peer-Peer communication matrix (unified):")
                 print(p2p_um_num_bytes_comm_matrix, p2p_um_num_times_comm_matrix)
 
                 h2d_et_memcpy_comm = H2DCudaMemcpyCommMatrixGenerator(num_devices)
@@ -31,10 +34,15 @@ class Matrix():
                 print("Peer-Peer communication (explicit) matrix:")
                 print(p2p_et_num_bytes_comm_matrix, p2p_et_num_times_comm_matrix)
 
-                
-        # all_um_num_bytes_comm_matrix = merge_matrices(h2d_um_num_bytes_comm_matrix, p2p_um_num_bytes_comm_matrix)
-        # all_um_num_times_comm_matrix = merge_matrices(h2d_um_num_times_comm_matrix, p2p_um_num_times_comm_matrix)
-    
+        for file_path in self.metric_trace_paths:
+            if (os.path.exists(file_path)):
+                zc_comm = ZeroCopyInfoGenerator(num_devices)
+                zc_num_bytes_comm_matrix, zc_num_times_comm_matrix = zc_comm.generate_comm_matrix(filepath=file_path)
+
+                print("Zero copy communication matrix:")
+                print(zc_num_bytes_comm_matrix, zc_num_times_comm_matrix)
+               
+
     
 
 class Node:
