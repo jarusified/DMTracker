c42a679e4a43b81168e00d733f1601cf8259276b
jarusified <jarus3001@gmail.com>
2022-09-25 11:18:39 -0700

    clean up unused pybind ops

--- dmv_profiler/lib/pybind.cpp
+++ dmv_profiler/lib/pybind.cpp
@@ -6,18 +6,6 @@ PYBIND11_MODULE(dmv_profiler, m)
 {
     m.doc() = "Data Movement profiler for CUDA";
 
-    // m.def(
-    //     "add_event",
-    //     [](std::string eventname, py::dict metadata)
-    //     {
-    //         nl::json json_metadata = metadata;
-    //         torch_rdu::JIT::jit().profiler().add_event(eventname, json_metadata);
-    //     },
-    //     py::arg("eventname") = py::none(), py::arg("metadata") = py::dict());
-    // m.def("end_profile", []()
-    //       { torch_rdu::JIT::jit().profiler().end_profile(); });
-    // m.def("save_profile", [](std::string filename)
-    //       { torch_rdu::JIT::jit().profiler().save_profile(filename); });
     m.def("start_profile", []() { 
         std::set<libkineto::ActivityType> types = {
             libkineto::ActivityType::CONCURRENT_KERNEL,
@@ -37,14 +25,4 @@ PYBIND11_MODULE(dmv_profiler, m)
         profiler.prepareTrace(types, profiler_config);
         profiler.startTrace(); 
     });
-    // m.def(
-    //     "start_event",
-    //     [](std::string eventname, py::dict metadata)
-    //     {
-    //         nl::json json_metadata = metadata;
-    //         torch_rdu::JIT::jit().profiler().start_event(eventname, json_metadata);
-    //     },
-    //     py::arg("eventname") = py::none(), py::arg("metadata") = py::dict());
-    // m.def("end_event", [](std::string eventname)
-    //       { torch_rdu::JIT::jit().profiler().end_event(eventname); });
 }
