c041da93c2bbc5b652f0bc455a689377fef0e418
jarusified <jarus3001@gmail.com>
2022-11-02 01:31:29 -0700

    Separate setup into two steps

--- dmv_profiler/build.sh
+++ dmv_profiler/build.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
+# TODO (surajk): Use this for python installation.
+
 # Clean up old build, if it exists.
 rm -rf CMakeFiles CMakeCache.txt compile_commands.json cmake_install.cmake Makefile libdmv.a
 
 # Load required module/version
 module load spectrum-mpi/rolling-release
-module load cuda/11.1.0
 module load cmake/3.16.8
 module load gcc/8.3.1
 module load python/3.8.2
@@ -18,3 +19,5 @@ cmake . -DCUDA_SOURCE_DIR=$CUDA_SOURCE_DIR -DFMT_SOURCE_DIR=$FMT_SOURCE_DIR -DGO
 
 export DMV_SOURCE_DIR=$(pwd)
 echo "DMV_SOURCE_DIR        = $DMV_SOURCE_DIR"
+
+cmake --build .
--- dmv_profiler/linux-setup.sh
+++ dmv_profiler/linux-setup.sh
@@ -31,26 +31,3 @@ echo "CUDA_SOURCE_DIR        = $CUDA_SOURCE_DIR"
 echo "FMT_SOURCE_DIR         = $FMT_SOURCE_DIR"
 echo "GOOGLETEST_SOURCE_DIR  = $GOOGLETEST_SOURCE_DIR"
 echo "PYBIND_SOURCE_DIR      = $PYBIND_SOURCE_DIR"
-
-# Clean up old build, if it exists.
-rm -rf CMakeFiles CMakeCache.txt compile_commands.json cmake_install.cmake Makefile libdmv.a
-
-# Load required module/version
-module load spectrum-mpi/rolling-release
-module load cmake/3.16.8
-module load gcc/8.3.1
-module load python/3.8.2
-
-# Force the compiler to pick the correct versions.
-export CC=`which gcc`
-export CXX=`which g++`
-
-# Compile the sources
-cmake . -DCUDA_SOURCE_DIR=$CUDA_SOURCE_DIR -DFMT_SOURCE_DIR=$FMT_SOURCE_DIR -DGOOGLETEST_SOURCE_DIR=$GOOGLETEST_SOURCE_DIR
-
-# Build!
-cmake --build .
-
-# Add repo to the env variable.
-export DMV_SOURCE_DIR=$(pwd)
-echo "DMV_SOURCE_DIR        = $DMV_SOURCE_DIR"
